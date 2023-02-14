f5668d54b3e67b02c0d0d6152b22020c2f8ecf2d
FedeAle1525 <fedeale.1525@gmail.com>
2023-02-14 15:10:49 +0100

    JS code for "Milestone 0"

---
+++ js/script.js
@@ -0,0 +1,47 @@
+// MILESTONE 0: Creare un array di Oggeti contenente i Membri del Team
+const team = [
+
+  {
+    firstName: 'Wayne',
+    lastName: 'Barnett',
+    role: 'Founder & CEO',
+    picture: './img/wayne-barnett-founder-ceo.jpg'
+  },
+
+  {
+    firstName: 'Angela',
+    lastName: 'Caroll',
+    role: 'Chief Editor',
+    picture: './img/angela-caroll-chief-editor.jpg'
+  },
+
+  {
+    firstName: 'Walter',
+    lastName: 'Gordon',
+    role: 'Office Manager',
+    picture: './img/walter-gordon-office-manager.jpg'
+  },
+
+  {
+    firstName: 'Angela',
+    lastName: 'Lopez',
+    role: 'Social Media Manager',
+    picture: './img/angela-lopez-social-media-manager.jpg'
+  },
+
+  {
+    firstName: 'Scott',
+    lastName: 'Estrada',
+    role: 'Developer',
+    picture: './img/scott-estrada-developer.jpg'
+  },
+
+  {
+    firstName: 'Barbara',
+    lastName: 'Ramos',
+    role: 'Graphic Designer',
+    picture: './img/barbara-ramos-graphic-designer.jpg'
+  },
+
+];
+
